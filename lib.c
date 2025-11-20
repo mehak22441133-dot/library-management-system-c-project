cf5e30f4c49f15278afc1f6f17c1b6d9bc64d561
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-11-20 23:09:49 +0530

    Refactor main function for user choice prompts

--- lib.c
+++ lib.c
@@ -1,9 +1,14 @@
 #include <stdio>
-#include 
+#include
 
-void main(){
-    int select ;
-    printf("select ur choice\n");
-    printf("")
-    printf()
+void main() {
+	int select ;
+	printf("select ur choice\n");
+	printf("press 1 for new registration of student\n");
+	printf("press 2 for issue books\n");
+	printf("pres 3 for returning books\n");
+	printf("pres 4 for add new books\n");
+	printf("pres 0 for exit program\n");
+	scanf("%d",&select);
+	switch()
 }
