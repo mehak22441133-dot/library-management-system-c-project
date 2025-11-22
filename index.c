e0e9ce0f1385416e8ea4cf948d66b17c4e931e25
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-11-22 23:12:35 +0530

    Refactor main function and add missing includes

---
+++ index.c
@@ -0,0 +1,52 @@
+
+#include <stdio>
+#include"registration.c"
+#include"issuebooks"
+#include"returnbooks"
+#include"addbooks"
+
+extern int registration ();
+extern int issuebooks();
+extern int returnbooks();
+extern int addbooks();
+
+void main() {
+	int select ;
+    do{
+	printf("select ur choice\n");
+	printf("press 1 for new registration of student\n");
+	printf("press 2 for issue books\n");
+	printf("pres 3 for returning books\n");
+	printf("pres 4 for add new books\n");
+	printf("pres 0 for exit program\n");
+	scanf("%d",&select);
+	switch(select)
+    {
+        case 1:
+        registration ();
+        printf("\n\n\n");
+        break;
+
+        case 2:
+        issuebooks();
+        printf("\n\n\n");
+        break;
+
+        case 3:
+        returnbooks();
+        printf("\n\n\n");
+        break;
+
+        case 4:
+        addbooks();
+        printf("\n\n\n");
+        break;
+
+        default:
+            printf("pls enter valid option\n");
+            break;     
+        }
+    }while(select!=0);  
+       printf("you are exit from program\n");
+        
+}
