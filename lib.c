a1e3238ba061a582d10488713a5c631a446c42fc
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-11-26 22:46:39 +0530

    Update and rename index.c to lib.c

---
+++ lib.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctime>
+#include<cstring>
+
+
+void heading();
+void admin();
+void user();
+void user_profile();
+void create_profile_user();
+void user_login();
+void create_profile_Admin();
+void admin_login();
+void user_menu();
+void deleteBook();
+void admin_menu ( );
+void help();
+void showbook();
+void showbook2();
+void findbook();
+void findbook2();
